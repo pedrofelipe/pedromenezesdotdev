76467c87ae5eb14569052d524bb15c7e0ae8b472
Pedro Menezes <pedro.menezes@kununu.com>
2019-02-28 21:59:16 +0000

    Hello World

---
+++ components/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './index.scss';
+
+class App extends Component {
+  render () {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+import App from '../components';
+
+function Main () {
+  return (
+    <>
+      <Head>
+        <title>Pedro Menezes</title>
+      </Head>
+      <App />
+    </>
+  )
+}
+
+export default Main;
