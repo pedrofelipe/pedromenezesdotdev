a5536493a8c0fe45f5fc881eb8a2d0418aeac135
Pedro Menezes <pedro.menezes@kununu.com>
2019-02-28 22:20:21 +0000

    Switch to dark mode; Add missing meta tags

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,11 @@ function Main () {
     <>
       <Head>
         <title>Pedro Menezes</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta property="og:title" content="Pedro Menezes" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://pedromenezes.dev" />
       </Head>
       <App />
     </>
